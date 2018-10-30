076a140fd48226fb97075cc3e3c803c90b576c3c
wangyb <wangyb@163.com>
2018-10-30 10:43:46 +0800

    quartz01

---
+++ quartz/src/main/java/com/example/quartz/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,35 @@
+package com.example.quartz;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+/**
+ * Created with Intellij IDEA.
+ *
+ * @author wangyb
+ * @Date 2018/10/30 10:41
+ * Modified By:
+ * Description:
+ */
+public class AutowiringSpringBeanJobFactory  extends SpringBeanJobFactory
+        implements ApplicationContextAware{
+    private transient AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+
+        beanFactory.autowireBean(job);
+
+        return job;
+    }
+}
